96db8e21263845abc71cb2cda47a85eb5348bb6a
Chris Oyler <christopher.oyler@gmail.com>
2015-04-23 09:01:28 -0700

    sell doesn't work

--- client/index.js
+++ client/index.js
@@ -2,61 +2,71 @@
 
 angular.module('day-trader', ['firebase'])
 .run(['$rootScope', '$window', function($rootScope, $window){
-  $rootScope.fbRoot = new $window.Firebase('https://daytrippin.firebaseio.com/');
+  $rootScope.fbRoot = new $window.Firebase('https://daytripper.firebaseio.com/');
 }])
 .controller('master', ['$scope', '$firebaseObject', '$firebaseArray', '$http', function($scope, $firebaseObject, $firebaseArray, $http){
   var fbUser = $scope.fbRoot.child('user');
   var afUser = $firebaseObject(fbUser);
   $scope.user = afUser;
   
-  var fbPortfolios = $scope.fbRoot.child('portfolios');
+  // var fbPortfolios = $scope.fbRoot.child('portfolios');
+  // var afPortfolios = $firebaseArray(fbPortfolios);
+  // $scope.portfolios = afPortfolios;
+  var fbPortfolios = $scope.fbRoot.child('technology');
   var afPortfolios = $firebaseArray(fbPortfolios);
-  $scope.portfolios = afPortfolios;
+  $scope.technology = afPortfolios;
+
+  var fbPortfolios2 = $scope.fbRoot.child('cereal');
+  var afPortfolios2 = $firebaseArray(fbPortfolios2);
+  $scope.cereal = afPortfolios2;
+
+  var fbPortfolios3 = $scope.fbRoot.child('pharma');
+  var afPortfolios3 = $firebaseArray(fbPortfolios3);
+  $scope.pharma = afPortfolios3;
   
   $scope.addUser = function(){
     $scope.user.$save();
     $scope.swapBar();
   };
+  $scope.buyShares = function(){
+    $http.jsonp('http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol='+$scope.stock.symbol+'&callback=JSON_CALLBACK').then(function(response){
+      var stock = {};
+      stock.amount = $scope.stock.amount;
+      stock.symbol = $scope.stock.symbol;
+      stock.price = (response.data.LastPrice);
+
+      if ($scope.user.capital - (stock.amount * stock.price) >= 0 && ($scope.stock.portfolio)){
+        $scope[$scope.stock.portfolio].$add(stock);
+        $scope.user.capital -= (stock.amount * stock.price);
+        $scope.user.$save();
+        $scope.portTotal(portfolio);
+        
+      } else {
+        alert('Check your input and try again..');
+      }
+      // $scope.newBalance(0,0);
+    });
+  };
   $scope.swapBar = function(){
     $scope.setupBar = true;
   };
-  $scope.addPortfolio = function(){
-    if($scope.portfolios.$id){
-      $scope.portfolios.$save($scope.portfolio).then(function(){
-        $scope.calcFunds();
-      });
-    }else{
-      $scope.portfolios.$add($scope.portfolio);
-    }
+  $scope.sellShares = function(stock, port){
+    $scope.user.capital += (stock.price * stock.amount);
+    $scope[port].$remove(stock);
+    $scope.user.$save();
+    $scope.portfolio.$save();
   };
-  $scope.buyShares = function(index){
-    
-    var shares = $scope.portfolio.stock.amount;
-    
-    $http.jsonp('http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol='+ $scope.portfolio.stock.symbol +'&callback=JSON_CALLBACK').then(function(response){
-      
-      
-      if((response.data.LastPrice * shares) < $scope.user.capital){
-        $scope.user.capital -= (response.data.LastPrice * shares);
-        
-        $scope.stock = {};
-        $scope.portfolio.stock.shares = shares;
-        $scope.portfolio.stock.price = response.data.LastPrice;
-        $scope.portfolio.stock.position= (response.data.LastPrice * shares);
-        console.log($scope.stock);
-        $scope.portfolios.$add($scope.stock);
-        $scope.user.$save($scope.user.capital);
-      
-      }else{
-        alert('Not Enough Funds');
-      }
+  // $scope.sellShares = function(stock, port){
+  //     $scope.user.balance += (stock.price * stock.amount);
+  //     $scope[port].$remove(stock);
+  //     //$scope.saveUser();
+  //   };
+  $scope.portTotal = function(portfolio){
+    var total = 0;
+    $scope[portfolio].forEach(function(current){
+      total += (current.price * current.amount);
     });
+    return total;
   };
-  $scope.sellShares = function(stock){
-    $scope.stock.$remove(stock).then(function(){
-      $scope.user.capital += (stock.price * stock.shares);
-      //$scope.portfolio.$remove(stock);
-      $scope.user.$save($scope.user.capital);
-    });
-  };
+
 }]);
