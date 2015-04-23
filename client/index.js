a3b205dad4da6f8c872b9f3a9775df367487bb72
Chris Oyler <christopher.oyler@gmail.com>
2015-04-23 08:06:57 -0700

    portfolio cards filling in

--- client/index.js
+++ client/index.js
@@ -21,35 +21,42 @@ angular.module('day-trader', ['firebase'])
     $scope.setupBar = true;
   };
   $scope.addPortfolio = function(){
-    $scope.portfolios.$add($scope.portfolio);
+    if($scope.portfolios.$id){
+      $scope.portfolios.$save($scope.portfolio).then(function(){
+        $scope.calcFunds();
+      });
+    }else{
+      $scope.portfolios.$add($scope.portfolio);
+    }
   };
   $scope.buyShares = function(index){
-
-    //var symbol = $scope.portfolio.stock.symbol;
+    
     var shares = $scope.portfolio.stock.amount;
-
+    
     $http.jsonp('http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol='+ $scope.portfolio.stock.symbol +'&callback=JSON_CALLBACK').then(function(response){
-    //console.log(response.data.LastPrice);
-        
-        
+      
+      
       if((response.data.LastPrice * shares) < $scope.user.capital){
         $scope.user.capital -= (response.data.LastPrice * shares);
         
         $scope.stock = {};
-        //$scope.stock.name = index;
         $scope.portfolio.stock.shares = shares;
-        //$scope.portfolio.stock.symbol = symbol;
         $scope.portfolio.stock.price = response.data.LastPrice;
         $scope.portfolio.stock.position= (response.data.LastPrice * shares);
         console.log($scope.stock);
         $scope.portfolios.$add($scope.stock);
-        //$scope.portfolios['0'].$add(stock.price);
-        //$scope.portfolio.name.$add($scope.stock.symbol);
-        //console.log(shares);
+        $scope.user.$save($scope.user.capital);
+      
       }else{
         alert('Not Enough Funds');
       }
     });
   };
-  
+  $scope.sellShares = function(stock){
+    $scope.stock.$remove(stock).then(function(){
+      $scope.user.capital += (stock.price * stock.shares);
+      //$scope.portfolio.$remove(stock);
+      $scope.user.$save($scope.user.capital);
+    });
+  };
 }]);
